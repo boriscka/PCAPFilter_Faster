1ae88c6e6b4573a270dbab375b45b2b0835c80d2
Borisenko Jura <j.borisenko@osnovalab.ru>
2019-07-29 18:10:24 +0300

    todo: remove it from history

--- Build/Linux_info.sh
+++
@@ -1,1 +0,0 @@
-sudo $RTE_SDK/share/dpdk/tools/dpdk-devbind.py -s
--- Build/Linux_init.sh
+++
@@ -1,5 +0,0 @@
-#sudo sysctl -w vm.nr_hugepages=1024
-sudo $RTE_SDK/share/dpdk/tools/dpdk-devbind.py -s
-sudo $RTE_SDK/share/dpdk/tools/dpdk-devbind.py --force --bind=igb_uio enp0s8
-sudo $RTE_SDK/share/dpdk/tools/dpdk-devbind.py --force --bind=igb_uio enp0s9
-sudo $RTE_SDK/share/dpdk/tools/dpdk-devbind.py -s
